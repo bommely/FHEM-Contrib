9e7638141a852225c202ca5d1520587a9e6b9e59
tsc <tsc@kuzo-media.de>
2015-07-22 04:07:46 +0200

    add define function with start

--- 98_SMA_EM.pm
+++ 98_SMA_EM.pm
@@ -8,7 +8,7 @@
 package main;
 
 use strict;
-use warning;
+use warnings;
 use IO::Socket::Multicast;
 
 # ##############################################################################
@@ -16,38 +16,74 @@ use IO::Socket::Multicast;
 # ##############################################################################
 sub SMA_EM_Initialize($) {
 	my ($hash) = @_;
-	$hash->{ReadFn} = "SMA_EM_Read";
-	$hash->{ReadyFn] = "SMA_EM_Ready";
+#	$hash->{ReadFn} = "SMA_EM_Read";
+#	$hash->{ReadyFn} = "SMA_EM_Ready";
 	$hash->{DefFn} = "SMA_EM_Define";
-	$hash->{UndefFn} = "SMA_EM_Undefine";
-	$hash->{GetFn} = "SMA_EM_Get";
-	$hash->{NotifyFn} = "SMA_EM_Notify";
+#	$hash->{UndefFn} = "SMA_EM_Undefine";
+#	$hash->{GetFn} = "SMA_EM_Get";
+#	$hash->{NotifyFn} = "SMA_EM_Notify";
 }
 
 # ##############################################################################
 # called when a module defined
 # ##############################################################################
 sub SMA_EM_Define($$) {
+	my ($hash, $def) = @_;
+	my @a = split(/\s+/, $def);
+	my $now  = TimeNow();
+	my $serialNumber = $a[2];
 	
+	$hash->{serialNumber} = $serialNumber;
+	#Log3($hash->{NAME}, 5, "Count Parameters: ".int(@a));
+	
+	InternalTimer(gettimeofday()+1, "SMA_EM_Parse", $hash, 0);
+	
+	return "Wrong syntax: use define <name> SMA_EM <serialnumber>" if(int(@a) < 3);
+
+	return undef;
 }
 
 # ##############################################################################
-# called when a module deleted
+# called after initialize
 # ##############################################################################
-sub SMA_EM_Undefine($$) {
+sub SMA_EM_Parse($) {
+	my ($hash, $a) = @_;
+	my $socket;
+	my $data;
+	$socket = IO::Socket::Multicast->new(LocalPort=>9522);
+	$socket->mcast_add('239.12.255.254');
+	while(1==1) {
+		#listen at multicast port
+	
+		$socket->recv($data, 600);
+	
+		readingsBeginUpdate($hash);
+		my $serialNumber = SMA_EM_GetData($data, 21, 24);
+		readingsBulkUpdate($hash, "PConsumptionAll", SMA_EM_GetData($data, 32, 36, 1)+0/10);
+		readingsBulkUpdate($hash, "PReturnAll", SMA_EM_GetData($data, 52, 56, 1)+0/10);
+		readingsEndUpdate($hash, 1);
+	}
+	$socket->mcast_drop('239.12.255.254');
+	
+	
 	
 }
 
-# ##############################################################################
-# called before FHEM shut down
-# ##############################################################################
-sub SMA_EM_Shutdown($$) {
-	
+sub SMA_EM_GetData() {
+	my($data, $startByte, $endByte, $debug)=@_;
+	my $return;
+	my $byte;
+	for($byte=0; $byte<length($data); $byte=$byte+1) {
+		if($byte >= $startByte-1 && $byte <=  $endByte-1) {
+			if($debug == 1) {
+				Log3 "SMA", 5, "SMA_GetData: byte: ".$byte.": ".sprintf("%02lx", ord substr($data, $byte, 1));
+			}
+			$return = $return.sprintf("%02lx", ord substr($data, $byte, 1));
+		}
+	}
+	Log3 'ABC', 5, hex($return);
+	return(hex($return));
 }
 
-# ##############################################################################
-# called when FHEM is ready
-# ##############################################################################
-sub SMA_EM_Ready($) {
-	
-}
+
+1;
